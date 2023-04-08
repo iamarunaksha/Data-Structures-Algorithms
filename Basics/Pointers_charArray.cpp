530633e424e0b54210ff08f17af80b1f4baf06b0
iamarunaksha <130035452+iamarunaksha@users.noreply.github.com>
2023-04-09 00:59:10 +0530

    Create Pointers_charArray.cpp

---
+++ Basics/Pointers_charArray.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int arr[5] = {1,2,3,4,5};
+    
+    char ch[6] = "abcde";         // Size of array should be always atleast 1 more for char arrays because of \0 null character at the end.
+
+    cout << arr << endl;         // Print the address of arr
+    //attention here
+    cout << ch << endl;         // Prints the entire string abcde
+    
+    char *c = &ch[0];
+    //Pointers also prints entire string, since at c address of ch is stored which when printed through cout returns the entire string bcoz of diff implementation of cout for char arrays
+    cout << c << endl;
+
+    char temp = 'z';
+    char *p = &temp;         
+
+    cout << p << endl;        // Prints unwanted charters till iterator finds a \0 character in the end
+
+    return 0;
+}
